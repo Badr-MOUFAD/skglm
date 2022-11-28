d93f82593914b674c523906efe018c54fb48d868
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-11-28 21:50:04 +0100

    jitclass aa

--- skglm/prototype_acd/utils.py
+++ skglm/prototype_acd/utils.py
@@ -5,11 +5,10 @@ from numba.experimental import jitclass
 
 
 @jitclass(
-    ('K', int32),
-    ('current_iter', int32),
-    ('arr_w_', float64[:]),
-    ('arr_Xw_', )
-
+    (('K', int32),
+     ('current_iter', int32),
+     ('arr_w', float64[:, ::1]),
+     ('arr_Xw', float64[:, ::1]))
 )
 class AndersonAcceleration:
     """Abstraction of Anderson Acceleration.
@@ -23,28 +22,27 @@ class AndersonAcceleration:
         Number of previous iterates to consider for extrapolation.
     """
 
-    def __init__(self, K):
+    def __init__(self, K, n_samples, n_features):
         self.K, self.current_iter = K, 0
-        self.arr_w_, self.arr_Xw_ = None, None
+        self.arr_w = np.zeros((n_features, K+1))
+        self.arr_Xw = np.zeros((n_samples, K+1))
 
     def extrapolate(self, w, Xw):
         """Return w, Xw, and a bool indicating whether they were extrapolated."""
-        if self.arr_w_ is None or self.arr_Xw_ is None:
-            self.arr_w_ = np.zeros((w.shape[0], self.K+1))
-            self.arr_Xw_ = np.zeros((Xw.shape[0], self.K+1))
 
         if self.current_iter <= self.K:
-            self.arr_w_[:, self.current_iter] = w
-            self.arr_Xw_[:, self.current_iter] = Xw
+            self.arr_w[:, self.current_iter] = w
+            self.arr_Xw[:, self.current_iter] = Xw
             self.current_iter += 1
             return w, Xw, False
 
-        U = np.diff(self.arr_w_, axis=1)  # compute residuals
+        # compute residuals
+        U = np.diff(self.arr_w)
 
         # compute extrapolation coefs
         try:
             inv_UTU_ones = np.linalg.solve(U.T @ U, np.ones(self.K))
-        except np.linalg.LinAlgError:
+        except Exception:
             return w, Xw, False
         finally:
             self.current_iter = 0
@@ -52,4 +50,75 @@ class AndersonAcceleration:
         # extrapolate
         C = inv_UTU_ones / np.sum(inv_UTU_ones)
         # floating point errors may cause w and Xw to disagree
-        return self.arr_w_[:, 1:] @ C, self.arr_Xw_[:, 1:] @ C, True
+        return self.arr_w[:, 1:] @ C, self.arr_Xw[:, 1:] @ C, True
+
+
+def test_anderson_acceleration():
+    from numpy.linalg import norm
+
+    # VAR: w = rho * w + 1 with |rho| < 1
+    # converges to w_star = 1 / (1 - rho)
+    max_iter, tol = 1000, 1e-9
+    n_features = 2
+    rho = np.array([0.5, 0.8])
+    w_star = 1 / (1 - rho)
+    X = np.diag([2, 5])
+
+    # with acceleration
+    acc = AndersonAcceleration(K=5, n_samples=X.shape[0], n_features=X.shape[1])
+    n_iter_acc = 0
+    w = np.ones(n_features)
+    Xw = X @ w
+    for i in range(max_iter):
+        w, Xw, _ = acc.extrapolate(w, Xw)
+        w = rho * w + 1
+        Xw = X @ w
+
+        if norm(w - w_star, ord=np.inf) < tol:
+            n_iter_acc = i
+            break
+
+    # without acceleration
+    n_iter = 0
+    w = np.ones(n_features)
+    for i in range(max_iter):
+        w = rho * w + 1
+
+        if norm(w - w_star, ord=np.inf) < tol:
+            n_iter = i
+            break
+
+    np.testing.assert_allclose(w, w_star)
+    np.testing.assert_allclose(Xw, X @ w_star)
+
+    print(n_iter_acc)
+    print(n_iter)
+    # np.testing.assert_array_equal(n_iter_acc, 13)
+    np.testing.assert_array_equal(n_iter, 99)
+
+
+if __name__ == "__main__":
+    import time
+
+    w1 = np.random.randn(10)
+    w2 = np.random.randn(5)
+
+    start = time.time()
+    aa1 = AndersonAcceleration(5, 10, 10)
+    aa1.extrapolate(w1, w1)
+    mid = time.time()
+    aa2 = AndersonAcceleration(5, 5, 5)
+    aa2.extrapolate(w2, w2)
+    end = time.time()
+
+    print(mid - start)
+    print(end - mid)
+
+    start = time.time()
+    aa1.extrapolate(w1, w1)
+    mid = time.time()
+    aa2.extrapolate(w2, w2)
+    end = time.time()
+
+    print(mid - start)
+    print(end - mid)
